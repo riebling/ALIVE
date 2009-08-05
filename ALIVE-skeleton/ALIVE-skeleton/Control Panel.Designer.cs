e983fd384f23073c2543b446eb896684e2c33538
er1k <er1k@a626feff-c0c0-40cf-90b3-84d169339bca>
2009-08-05 18:38:28 +0000

    Initial commit

---
+++ ALIVE-skeleton/ALIVE-skeleton/Control Panel.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace AliveControlPanel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.LogoutButton = new System.Windows.Forms.Button();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.MoveForwardButton = new System.Windows.Forms.Button();
+            this.NameBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(44, 47);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(210, 164);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.LoginButton.Location = new System.Drawing.Point(338, 79);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(100, 23);
+            this.LoginButton.TabIndex = 6;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            // 
+            // LogoutButton
+            // 
+            this.LogoutButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.LogoutButton.Location = new System.Drawing.Point(338, 108);
+            this.LogoutButton.Name = "LogoutButton";
+            this.LogoutButton.Size = new System.Drawing.Size(100, 23);
+            this.LogoutButton.TabIndex = 4;
+            this.LogoutButton.Text = "Logout";
+            this.LogoutButton.UseVisualStyleBackColor = true;
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(272, 52);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(35, 13);
+            this.NameLabel.TabIndex = 6;
+            this.NameLabel.Text = "Name";
+            // 
+            // MoveForwardButton
+            // 
+            this.MoveForwardButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.MoveForwardButton.Location = new System.Drawing.Point(338, 188);
+            this.MoveForwardButton.Name = "MoveForwardButton";
+            this.MoveForwardButton.Size = new System.Drawing.Size(100, 23);
+            this.MoveForwardButton.TabIndex = 9;
+            this.MoveForwardButton.Text = "Move Forward 1m";
+            this.MoveForwardButton.UseVisualStyleBackColor = true;
+            this.MoveForwardButton.Click += new System.EventHandler(this.MoveForwardButton_Click);
+            // 
+            // NameBox
+            // 
+            this.NameBox.DisplayMember = "One";
+            this.NameBox.FormattingEnabled = true;
+            this.NameBox.Items.AddRange(new object[] {
+            "Dog One",
+            "Dog Two",
+            "Dog Three",
+            "Dog Four",
+            "Dog Five",
+            "Dog Six"});
+            this.NameBox.Location = new System.Drawing.Point(317, 52);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(121, 21);
+            this.NameBox.TabIndex = 11;
+            this.NameBox.ValueMember = "Six";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 266);
+            this.Controls.Add(this.NameBox);
+            this.Controls.Add(this.MoveForwardButton);
+            this.Controls.Add(this.NameLabel);
+            this.Controls.Add(this.LogoutButton);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "ALIVE Control Panel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Button LogoutButton;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Button MoveForwardButton;
+        private System.Windows.Forms.ComboBox NameBox;
+
+    }
+}
+
---
+++ ALIVE-skeleton/ALIVE-skeleton/Control Panel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ALIVE;
+
+namespace AliveControlPanel
+{
+    public partial class Form1 : Form
+    {
+        public SmartDog Fido = null;
+
+        public Form1()
+        {
+            // Initialize the GUI 
+            InitializeComponent();
+
+            // Register button click callbacks
+            LoginButton.Click += new EventHandler(LoginButton_Click);
+        }
+
+        private void ParseNames(out string fn, out string ln, out string pw)
+        {
+            fn = ""; ln = ""; pw = "";
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            string fn, ln, pw;
+
+            ParseNames(out fn, out ln, out pw);
+            Fido = new SmartDog(fn, ln, pw);
+            if (!Fido.Login())
+                MessageBox.Show("Login failed\r\n" + 
+                    "Perhaps that user is already logged in?");
+        }
+
+        private void LogoutButton_Click(object sender, EventArgs e)
+        {
+            if (Fido != null)
+                Fido.Logout();
+        }
+
+        private void MoveForwardButton_Click(object sender, EventArgs e)
+        {
+            if (!Fido.GoForward(1))
+                MessageBox.Show("Failed to move forward\r\n" +
+                    "Perhaps you hit an obstacle?");
+        }
+        
+        }
+}
